1e49f10786f7152c176f1c8d98e27c183b2f2b83
Iulia <iuliaszentes@gmail.com>
2026-05-31 18:09:20 +0300

    run with vm and code generation

--- run_commands.sh
+++ run_commands.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-gcc src/run_main.c src/lexer.c src/parser.c src/ad.c src/at.c src/utils.c -o run_main
+gcc src/run_main.c src/lexer.c src/parser.c src/ad.c src/at.c src/vm.c src/gc.c src/utils.c -o run_main
 ./run_main "$1" > outputs/output_test.txt
